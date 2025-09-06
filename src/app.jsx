9960032d16d19ad04735ad1832d553d7c28d6985
Gonzathegon <axelmoai@gmail.com>
2025-09-06 17:50:21 -0300

    este boton no funca

--- src/app.jsx
+++ src/app.jsx
@@ -2,13 +2,21 @@
 
 
 export const App=()=> {
-  
-
+  const saludo="aca andamo programando de pana";
+   const mostrareventoimagen=()=>{
+    console.log("te has unido a los tralaleros");
+  };
   return (
     <>
-     aca estamos en el primer componente
+    
+    <p     className="texto" >{saludo}</p>
+    <h1>aca andamo de pana</h1>
+    <img src="public/tralalero-tralala.jpg" alt="tralalero por siempre"/>
+
+    <input type="text" />
+    <button onClick={mostrareventoimagen}>tralalero</button>
     </>
-  )
-}
+  );
+};
 
 

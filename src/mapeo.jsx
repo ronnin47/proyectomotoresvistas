a01ff02f4eecd1437c4ec78517108d4dae376b73
jorge tournier <tempesttempest66l@gmail.com>
2025-09-09 16:05:52 -0300

    tcomiteo tralalero

--- src/mapeo.jsx
+++ src/mapeo.jsx
@@ -3,6 +3,7 @@ import Card from 'react-bootstrap/Card';
 
 
 export const Mapeo = () => {
+
   const coleccion = [
     {
       nombre: "java",
@@ -26,19 +27,116 @@ export const Mapeo = () => {
         imagenurl: "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg",
         paginaoficial: "https://developer.mozilla.org/es/docs/Web/CSS",
     },
+     {
+      nombre: "java",
+      descripcion: "lenguaje de programacion",
+      imagenurl: "https://upload.wikimedia.org/wikipedia/en/3/30/Java_programming_language_logo.svg",
+      paginaoficial: "https://www.java.com/es/",
+    }
+    ,{
+        nombre: "javascript",
+        descripcion: "lenguaje de programacion",
+        imagenurl: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+        paginaoficial: "https://developer.mozilla.org/es/docs/Web/JavaScript",
+    },
+    {  nombre: "html",
+        descripcion: "lenguaje de maquetacion",
+        imagenurl: "https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg",
+        paginaoficial: "https://developer.mozilla.org/es/docs/Web/HTML",
+    },
+    {  nombre: "css",
+        descripcion: "lenguaje de estilos",
+        imagenurl: "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg",
+        paginaoficial: "https://developer.mozilla.org/es/docs/Web/CSS",
+    },
+     {
+      nombre: "java",
+      descripcion: "lenguaje de programacion",
+      imagenurl: "https://upload.wikimedia.org/wikipedia/en/3/30/Java_programming_language_logo.svg",
+      paginaoficial: "https://www.java.com/es/",
+    }
+    ,{
+        nombre: "javascript",
+        descripcion: "lenguaje de programacion",
+        imagenurl: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+        paginaoficial: "https://developer.mozilla.org/es/docs/Web/JavaScript",
+    },
+    {  nombre: "html",
+        descripcion: "lenguaje de maquetacion",
+        imagenurl: "https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg",
+        paginaoficial: "https://developer.mozilla.org/es/docs/Web/HTML",
+    },
+    {  nombre: "css",
+        descripcion: "lenguaje de estilos",
+        imagenurl: "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg",
+        paginaoficial: "https://developer.mozilla.org/es/docs/Web/CSS",
+    },
+     {
+      nombre: "java",
+      descripcion: "lenguaje de programacion",
+      imagenurl: "https://upload.wikimedia.org/wikipedia/en/3/30/Java_programming_language_logo.svg",
+      paginaoficial: "https://www.java.com/es/",
+    }
+    ,{
+        nombre: "javascript",
+        descripcion: "lenguaje de programacion",
+        imagenurl: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+        paginaoficial: "https://developer.mozilla.org/es/docs/Web/JavaScript",
+    },
+    {  nombre: "html",
+        descripcion: "lenguaje de maquetacion",
+        imagenurl: "https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg",
+        paginaoficial: "https://developer.mozilla.org/es/docs/Web/HTML",
+    },
+    {  nombre: "css",
+        descripcion: "lenguaje de estilos",
+        imagenurl: "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg",
+        paginaoficial: "https://developer.mozilla.org/es/docs/Web/CSS",
+    },
+     {
+      nombre: "java",
+      descripcion: "lenguaje de programacion",
+      imagenurl: "https://upload.wikimedia.org/wikipedia/en/3/30/Java_programming_language_logo.svg",
+      paginaoficial: "https://www.java.com/es/",
+    }
+    ,{
+        nombre: "javascript",
+        descripcion: "lenguaje de programacion",
+        imagenurl: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+        paginaoficial: "https://developer.mozilla.org/es/docs/Web/JavaScript",
+    },
+    {  nombre: "html",
+        descripcion: "lenguaje de maquetacion",
+        imagenurl: "https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg",
+        paginaoficial: "https://developer.mozilla.org/es/docs/Web/HTML",
+    },
+    {  nombre: "css",
+        descripcion: "lenguaje de estilos",
+        imagenurl: "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg",
+        paginaoficial: "https://developer.mozilla.org/es/docs/Web/CSS",
+    },
   ];
+
 return (
-  <div>
-    {coleccion.map((lenguaje, index) => (
-      <Carta key={index}
-        nombre={lenguaje.nombre}
-        descripcion={lenguaje.descripcion}
-        imagenurl={lenguaje.imagenurl}
-        paginaoficial={lenguaje.paginaoficial}>
-        
-      </Carta>
-    ))}
-  </div>
+  <>
+
+      <div className="mapeoCards">
+
+        {coleccion.map((lenguaje, index) => (
+            <Carta key={index}
+              nombre={lenguaje.nombre}
+              descripcion={lenguaje.descripcion}
+              imagenurl={lenguaje.imagenurl}
+              paginaoficial={lenguaje.paginaoficial}>
+              
+            </Carta>
+          ))}
+          
+      </div>
+  
+  
+  
+  </>
 );
 }
 
@@ -47,8 +145,14 @@ const Carta=({nombre, descripcion, imagenurl, paginaoficial})=> {
 
 
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={imagenurl} />
+    <Card style={{ width: '14rem' }}>
+      <Card.Img variant="top" src={imagenurl}  style={{
+          width: '100%',
+          height: '220px',      // Tamaño fijo para todas las imágenes
+          objectFit: 'contain', // Mantiene la proporción y muestra todo el logo
+          background: '#fff',
+          padding: '12px'
+        }}/>
       <Card.Body>
         <Card.Title>{nombre}</Card.Title>
         <Card.Text>
